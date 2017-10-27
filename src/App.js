d91341edecfc7be75fdd3bf5ea1a1edc242aa2ee
michaelvincerra <michael.vincerra@gmail.com>
2017-10-26 20:59:49 -0700

    Annotates App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
+// Learn to use components
+
 class App extends Component {
   render () {
     return (
